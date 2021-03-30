4a347d6f8bb937d5838e591cc2278610c4342168
Smikkel20 <43788333+Smikkel20@users.noreply.github.com>
2021-03-30 17:24:23 +0200

    the wip bot
    the new bot that is still a wip.

---
+++ General.py
@@ -0,0 +1,69 @@
+import os
+import discord
+import random
+import praw
+import sys
+import validators
+from discord.ext.commands import Bot
+from discord.ext import commands
+from discord import Color
+from dotenv import load_dotenv
+
+bot = commands.Bot(command_prefix = "!")
+bot.remove_command("help")
+
+if __name__ == "__main__":
+    #get every file in ./cogs dir
+    for filename in os.listdir("./cogs"):
+        #check if the filename ends with .py
+        if filename.endswith(".py"):
+            try:
+                #load <filename>
+                bot.load_extension(f"cogs.{filename[:-3]}")
+            except Exception as e:
+                print(f"failed to load extension {filename}", file=sys.stderr)
+
+load_dotenv()
+#get tokens from .env file
+TOKEN = os.getenv("TEST_TOKEN")
+RedditPassword = os.getenv("REDDIT_PASSWORD")
+
+@bot.event
+async def on_ready():
+    print("---------------")
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    #Sets activity 
+    await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="twitch.tv/JunJiRah"))
+    print('activity set')
+    print('---------------')
+
+@bot.event
+async def on_command_error(ctx, error):
+    if isinstance(error, commands.errors.CommandNotFound):
+        return
+    else:
+        raise error
+
+@bot.command()
+async def ping(ctx):
+    await ctx.send(f'Pong! {bot.latency} ms!')
+
+@bot.command()
+async def reload(ctx):
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            try:
+                #reload extensions
+                bot.unload_extension(f"cogs.{filename[:-3]}")
+                bot.load_extension(f"cogs.{filename[:-3]}")
+                await ctx.send(f"Reloaded {filename[:-3]}")
+            except Exception as e:
+                print(f"failed to reload extension cogs.{filename[:-3]}", file=sys.stderr)
+
+
+
+
+
+bot.run(TOKEN)
---
+++ music.py
@@ -0,0 +1,134 @@
+import discord
+import os
+import random
+import youtube_dl
+import validators
+import re
+import urllib
+import asyncio
+from discord.ext.commands import Bot
+from discord.ext import commands
+
+queue = []
+
+def searchurl(url):
+    if not validators.url(url) == True:
+        query_string = urllib.parse.urlencode({
+            'search_query': url
+        })
+        print(query_string)
+        htm_content = urllib.request.urlopen(
+            'http://www.youtube.com/results?' + query_string
+        )
+        search_content= htm_content.read().decode()
+        url = re.findall(r'\/watch\?v=\w+', search_content)
+        url = f"https://www.youtube.com{url[0]}"
+        print(url)
+        return url
+
+class Musiccommand(commands.Cog):
+    def __init__(self, bot, *args, **kwargs):
+        self.bot = bot
+        self.queue = asyncio.Queue()
+
+
+    @commands.command()
+    async def play(self, ctx, *url):
+        global queue
+        print(url)
+        url = str(url)
+        url = url.replace(" ","+")
+        print(url)
+        url = url[2:-2]
+        print(url)
+        song_there = os.path.isfile("song.mp3")
+        try:
+            if song_there:
+                os.remove("song.mp3")
+        except PermissionError:
+            if not validators.url(url) == True:
+                url = searchurl(url)
+            queue.append(url)
+            #await ctx.send("Wait for the current playing music to end or use the 'stop' command")
+            return
+        #Check if user is in vc
+        voice_state = ctx.author.voice
+        if voice_state is None:
+            return await ctx.send('You need to be in a voice channel to use this command')
+        #get the user his channel and the voice_client of the guild
+        channel = ctx.author.voice.channel
+        voice_client = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        #check that the bot isnt already connected to a vc in that guild
+        if not voice_client == None:
+            #check if the bot isnt connected to a your vc
+            if not channel == voice_client.channel:
+                return await ctx.send('im already connected to another vc')
+        else:
+            await channel.connect()
+        if not validators.url(url) == True:
+            url = searchurl(url)
+        print(url)
+        #get the voice again after connection
+        voice = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        ydl_opts = {
+            'format': 'bestaudio/best',
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '192',
+            }],
+        }
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        for file in os.listdir("./"):
+            if file.endswith(".mp3"):
+                os.rename(file, "song.mp3")
+        voice.play(discord.FFmpegPCMAudio("song.mp3"))
+    
+    @commands.command()
+    async def pause(self, ctx):
+        voice = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        if voice.is_playing():
+            voice.pause()
+        else:
+            await ctx.send("Currently no audio is playing.")
+
+
+    @commands.command()
+    async def resume(self, ctx):
+        voice = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        if voice.is_paused():
+            voice.resume()
+        else:
+            await ctx.send("The audio is not paused.")
+
+
+    @commands.command()
+    async def stop(self, ctx):
+        global queue
+        voice = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        voice.stop()
+        queue = []
+        #id = ctx.message.guild.id
+        #if os.path.exists(f"./queue/queue{id}.csv"):
+            #os.remove(f"./queue/queue{id}.csv")
+
+    @commands.command(name="queue")
+    async def queue_(self,ctx):
+        global queue
+        await ctx.send(f"the songs in queue are: `{queue}`")
+
+    @commands.command()
+    async def replay(self, ctx):
+        voice = discord.utils.get(ctx.bot.voice_clients, guild=ctx.guild)
+        try:
+            voice.play(discord.FFmpegPCMAudio("song.mp3"))
+        except Exception:
+            await ctx.send("Couldnt find last song")
+        
+
+
+
+def setup(bot):
+    bot.add_cog(Musiccommand(bot))
+    print("Music has been loaded")
